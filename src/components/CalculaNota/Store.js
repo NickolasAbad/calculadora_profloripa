42be7e1a2ec643d5ab70947d2ae8938bcaeec7b9
Nickolas Abad <123907645+NickolasAbad@users.noreply.github.com>
2023-11-29 16:07:29 -0300

    Atualizar o Store.js

--- src/components/CalculaNota/Store.js
+++ src/components/CalculaNota/Store.js
@@ -68,14 +68,14 @@ class Store {
         function calcularPontuacao(NP, NTPC, NPC, NPI, gabaritoCandi, gabaritoQuest) {
 
             const valorInicial = 0;
-
-				if(NP === 8 && gabaritoCandi === gabaritoQuest) {
-                    valorNota.push(1)
-                } else if (NP === 8 && gabaritoCandi !== gabaritoQuest) {
-                    valorNota.push(0)
-                } else {
-                	for (let b = 0; b < NP.length; b++) {       
-                    	if(NPC[b] > NPI[b]) {
+            
+                	for (let b = 0; b < NP.length; b++) {
+                        
+                    if(NP[b] === 8 && gabaritoCandi[b] === gabaritoQuest[b]) {
+                    	valorNota.push(1)
+                		} else if (NP[b] === 8 && gabaritoCandi[b] !== gabaritoQuest[b]) {
+                    		valorNota.push(0)
+                		} else if(NPC[b] > NPI[b]) {
                         	if (isNaN(parseFloat((((NP[b] - (NTPC[b] - (NPC[b] - NPI[b]))) / NP[b])).toFixed(2)))) {
                             	valorNota.push(0)
                     	 	} else {
@@ -85,7 +85,6 @@ class Store {
                         	valorNota.push(0)
                     	}
             	}
-            }
             
             notasReunidas = parseFloat(valorNota.reduce(
                 (accumulator, currentValue) => accumulator + currentValue,
