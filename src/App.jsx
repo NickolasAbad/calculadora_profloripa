3a8a15a16e01e201872ac029725eaff3535bf310
Nickolas <nickolasabad51@gmail.com>
2023-12-09 02:34:49 -0300

    ga-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import CalculaNota from './components/CalculaNota/CalculaNota'
 import store from './components/CalculaNota/Store'
 
-import ReactGA from "react-ga"
+import ReactGA from "react-ga4";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const TRACKING_ID = "UA-190303794-1";
+const TRACKING_ID = "G-G9K4133B64";
 ReactGA.initialize(TRACKING_ID)
 
 function App() {
